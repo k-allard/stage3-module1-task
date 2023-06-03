2aae4651bba3e82237659a1bd2dacbb439f681ad
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-06-03 18:02:35 +0400

    Remove unused tests

--- module-main/src/test/java/com/mjc/school/ModuleMainApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.mjc.school;
-
-class ModuleMainApplicationTests {
-
-	void contextLoads() {
-	}
-
-}
--- module-repository/src/test/java/com/mjc/school/repository/ModuleRepositoryApplicationTests.java
+++
@@ -1,5 +0,0 @@
-package com.mjc.school.repository;
-
-class ModuleRepositoryApplicationTests {
-
-}
